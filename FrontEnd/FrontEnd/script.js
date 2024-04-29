c5454362cd8cd13bf45af6b176553d030cdb79f9
pmoharana-cmd <pmoharana032474@gmail.com>
2024-04-29 19:53:36 -0400

    Attempt changes

--- FrontEnd/FrontEnd/script.js
+++ FrontEnd/FrontEnd/script.js
@@ -1,23 +1,37 @@
 document.getElementById('userForm').addEventListener('submit', function(e) {
     e.preventDefault();
+  
     const formData = {
-        age: document.getElementById('age').value,
-        gpa: document.getElementById('gpa').value,
-        financial_aid: document.getElementById('financial_aid').value
+      age: parseInt(document.getElementById('age').value),
+      gpa: parseFloat(document.getElementById('gpa').value),
+      financial_aid: parseInt(document.getElementById('financial_aid').value)
     };
-    
-    fetch('https://us-central1-outstanding-map-421217.cloudfunctions.net/insert_data', {
-        method: 'POST',
-        mode: 'no-cors', // Added to bypass CORS issues for development
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
+
+    console.log(JSON.stringify(formData))
+  
+    fetch('https://us-east1-outstanding-map-421217.cloudfunctions.net/insert_data', {
+      method: 'POST',
+      mode: 'no-cors', // Added to bypass CORS issues for development
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(formData),
     })
     .then(response => {
-        console.log('Success:', response); // Note: response will be opaque due to no-cors
+      // Check the response status
+      if (response.ok) {
+        // Display success message
+        alert('Form submitted successfully!', 'success');
+      } else {
+        // Display error message
+        alert('Failed to submit the form. Please try again.', 'error');
+      }
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error('Error:', error);
+      // Display error message
+      alert('An error occurred while submitting the form.', 'error');
     });
-});
+  });
+  
