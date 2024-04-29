3ae1d2f50be768a8790aa680de982ffb96fd0464
adamd <adamdoyle630@gmail.com>
2024-04-29 11:19:08 -0400

    Update HTML form to include financial aid

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,12 +2,12 @@ document.getElementById('userForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const formData = {
         age: document.getElementById('age').value,
-        gpa: document.getElementById('gpa').value
+        gpa: document.getElementById('gpa').value,
+        financial_aid: document.getElementById('financial_aid').value,
     };
     
-    fetch('https://us-central1-outstanding-map-421217.cloudfunctions.net/function-1', {
+    fetch('https://us-east1-outstanding-map-421217.cloudfunctions.net/insert_data', {
         method: 'POST',
-        mode: 'no-cors', // Added to bypass CORS issues for development
         headers: {
             'Content-Type': 'application/json',
         },
